d6831accac475d8cfa70b804d1228e72ad2d35ec
DiateAtendimento <diate.rpps@gmail.com>
2025-06-04 11:41:35 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-// server.js
-
 const express = require('express');
+const cors = require('cors');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Permite ler JSON do corpo das requisições
+app.use(cors());
 app.use(express.json());
 
-// Endpoint para receber o POST do seu formulário
 app.post('/api/avaliacao', async (req, res) => {
   try {
     const dados = req.body;
 
-    // Faz o POST para o seu Google Apps Script
     const resposta = await fetch('https://script.google.com/macros/s/AKfycbwm_NNASjUjQwXbpGeNqlB4dj-6uyWJ28DYY50LzdwXwz8KqQm6SnKE74dhI1wWcroZ/exec', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -28,7 +25,6 @@ app.post('/api/avaliacao', async (req, res) => {
   }
 });
 
-// Inicia o servidor
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
